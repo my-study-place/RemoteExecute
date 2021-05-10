e9736350b92d9308414a706a64af6023b81a3721
Ryu JuHeon <saidbysolo@gmail.com>
2021-05-10 23:00:20 +0900

    fix(request): getOutputStream

--- src/main/java/me/saidbysolo/remoteexecute/Request.java
+++ src/main/java/me/saidbysolo/remoteexecute/Request.java
@@ -1,5 +1,6 @@
 package me.saidbysolo.remoteexecute;
 
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -11,26 +12,30 @@ public class Request {
         this.plugin = plugin;
     }
 
-    public int makeJsonForm(String id, int point, String game, String key) {
+    public byte[] makeJsonForm(String id, int point, String game, String key) {
         byte[] out = String.format("{\"id\": %s, \"point\": %d, \"game\": %s, \"key\":%s}", id, point, game, key)
                 .getBytes(StandardCharsets.UTF_8);
-        return out.length;
+        return out;
     }
 
-    public void requestPostJson(String stringUrl, int length) {
+    public void requestPostJson(String stringUrl, byte[] out) {
         try {
             URL url = new URL(stringUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setConnectTimeout(3000);
             connection.setReadTimeout(3000);
-            connection.setFixedLengthStreamingMode(length);
+            connection.setFixedLengthStreamingMode(out.length);
             connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             connection.connect();
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(out);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
         ;
-
     }
 }
